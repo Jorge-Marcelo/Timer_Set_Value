84c495a1101e6f294c4235753a2c88addb5315d4
Jorge-Marcelo <jorge3258@hotmail.com>
2023-08-12 22:12:24 -0300

    Comentando codigo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+/*
+Autor: Jorge Marcelo
+Data de Criação: 12/08/2023
+Github: https://github.com/Jorge-Marcelo
+Linkedin: https://www.linkedin.com/in/jorge-marcelo-067a5017b/
+Instagram: https://www.instagram.com/10_stronger/
+Threads: https://www.threads.net/@10_stronger
+*/ 
+
+
+/*Pega os elemntos HTML com DOM*/
 let startButton = document.getElementById("start");
 let finishButton = document.getElementById("finish");
 let display = document.getElementById("timer");
@@ -7,21 +18,29 @@ let segundosInput = document.getElementById("segundos");
 let duracao = 0;
 let timerInterval;
 
+/*Função de start do timer*/
 function start(evento) {
 evento.preventDefault();
 clearInterval(timerInterval);
 
+/*Calculo de horas minutos e segundos em milessimos de segundos
+PORQUE É ASSIM QUE O setInterval FUNCIONA*/
 duracao = (parseInt(horasInput.value) * 60 * 60) +
 (parseInt(minutosInput.value) * 60) +
 parseInt(segundosInput.value);
 timerInterval = setInterval(atualizarTimer, 1000);
 }
 
+/*Função que atualiza o timer*/
 function atualizarTimer() {
+/*Faz o calculo a partir da duração que foi setada 
+pelo usuario*/
 let horas = Math.floor((duracao / 60) / 60);
 let minutos = Math.floor(duracao / 60) - (horas * 60);
 let segundos = duracao % 60;
 
+
+/*Condicionais para adicionar um 0 a esquerda*/
 if (horas < 10) {
 horas = "0" + horas;
 }
@@ -34,16 +53,22 @@ if (segundos < 10) {
 segundos = "0" + segundos;
 }
 
+/*Mostra o conteudo das variaveis do  timer no HTML*/
 display.textContent = `${horas}:${minutos}:${segundos}`;
+
+/*Decrementa os valores do dsiplay*/
 duracao--;
 }
 
+/*Zera o timer*/
 function reset(evento) {
 evento.preventDefault();
 clearInterval(timerInterval);
+/*Mostra o timer zerado no HTML*/
 display.textContent = "00:00:00";
 }
 
+/*Escutador de eventos dos botões de start e reset*/
 startButton.addEventListener("click", start);
 finishButton.addEventListener("click", reset);
 
